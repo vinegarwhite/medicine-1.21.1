db9b8bf9641b2f5b9a710ccd5e6e2f73c55675f3
rageniv <1311763909@qq.com>
2024-09-28 21:49:43 +0800

    改进了钓竿谓词的写法 现在不需要给每一种钓竿都写专属的cast了 之前写法好傻

--- src/main/java/com/medicine/predicate/MedicineModelPredicate.java
+++ src/main/java/com/medicine/predicate/MedicineModelPredicate.java
@@ -7,69 +7,39 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.FishingRodItem;
 import net.minecraft.util.Identifier;
 
+/**
+ * 该类用于自定义谓词(Predicate) 可以在物品的model文件中使用 根据谓词切换不同的材质
+ * 原版的类叫做ModelPredicateProviderRegistry 可转跳过去进行参考
+ * 感谢某位大佬的帮助 我才知道谓词是存放那个类里的 才能以此为基础写自己的谓词
+ */
 public class MedicineModelPredicate {
 
     static {
-        ModelPredicateProviderRegistry.register(MedicineItems.FIBERGLASS_FISHING_ROD,Identifier.of(Medicine.MOD_ID, "fiberglass_fishing_rod_cast"),
+        // 根据原版的cast谓词改进 抛竿时谓词fishing_rod_cast变为1 配合json文件可以切换抛竿和收杆的材质
+        ModelPredicateProviderRegistry.register(Identifier.of(Medicine.MOD_ID, "fishing_rod_cast"),
+                // 根据个人理解 整个函数会对所有物品一次进行一次判断
                 (stack, world, entity, seed) -> {
+                    // 判断实体是否是玩家 如果不是则不会切换材质
                     if (entity == null) {
                         return 0.0F;
                     } else {
+                        // 判断该物品是否是玩家主手的物品
                         boolean bl = entity.getMainHandStack() == stack;
+                        // 判断该物品是否是玩家副手的物品
                         boolean bl2 = entity.getOffHandStack() == stack;
-                        if (entity.getMainHandStack().getItem() instanceof FishingRodItem) {
+                        // 因为双手都有物品会优先使用主手的物品 所以如果主手物品符合条件 则副手的cast为0不进行材质切换
+                        if (entity.getMainHandStack().getItem() == stack.getItem()) {
+                            // 判断到副手的物品是bl肯定为false 将bl2设为false则bl || bl2的结果必为false cast返回0
                             bl2 = false;
                         }
-
-                        return (bl || bl2) && entity instanceof PlayerEntity && ((PlayerEntity) entity).fishHook != null ? 1.0F : 0.0F;
-                    }
-                });
-        ModelPredicateProviderRegistry.register(MedicineItems.CARBON_FIBER_FISHING_ROD,Identifier.of(Medicine.MOD_ID, "carbon_fiber_fishing_rod_cast"),
-                (stack, world, entity, seed) -> {
-                    if (entity == null) {
-                        return 0.0F;
-                    } else {
-                        boolean bl = entity.getMainHandStack() == stack;
-                        boolean bl2 = entity.getOffHandStack() == stack;
-                        if (entity.getMainHandStack().getItem() instanceof FishingRodItem) {
-                            bl2 = false;
-                        }
-
-                        return (bl || bl2) && entity instanceof PlayerEntity && ((PlayerEntity) entity).fishHook != null ? 1.0F : 0.0F;
-                    }
-                });
-        ModelPredicateProviderRegistry.register(MedicineItems.TITANIUM_ALLOY_FISHING_ROD,Identifier.of(Medicine.MOD_ID, "titanium_alloy_fishing_rod_cast"),
-                (stack, world, entity, seed) -> {
-                    if (entity == null) {
-                        return 0.0F;
-                    } else {
-                        boolean bl = entity.getMainHandStack() == stack;
-                        boolean bl2 = entity.getOffHandStack() == stack;
-                        if (entity.getMainHandStack().getItem() instanceof FishingRodItem) {
-                            bl2 = false;
-                        }
-
-                        return (bl || bl2) && entity instanceof PlayerEntity && ((PlayerEntity) entity).fishHook != null ? 1.0F : 0.0F;
-                    }
-                });
-        ModelPredicateProviderRegistry.register(MedicineItems.SUPER_INVINCIBLE_FISHING_ROD,Identifier.of(Medicine.MOD_ID, "super_invincible_fishing_rod_cast"),
-                (stack, world, entity, seed) -> {
-                    if (entity == null) {
-                        return 0.0F;
-                    } else {
-                        boolean bl = entity.getMainHandStack() == stack;
-                        boolean bl2 = entity.getOffHandStack() == stack;
-                        if (entity.getMainHandStack().getItem() instanceof FishingRodItem) {
-                            bl2 = false;
-                        }
-
+                        // 物品位于某只手且实体为玩家且fishHook(与该玩家绑定的浮标实体)存在时 对应物品的cast为1 切换为抛竿的材质
                         return (bl || bl2) && entity instanceof PlayerEntity && ((PlayerEntity) entity).fishHook != null ? 1.0F : 0.0F;
                     }
                 });
     }
 
+    // 外部调用该函数 则该类内部的静态部分会自动执行 实现谓词(Predicate)的注册
     public static void registerMedicineModelPredicate(){
-
         Medicine.LOGGER.info("Registering Medicine Model Predicate");
     }
 }
