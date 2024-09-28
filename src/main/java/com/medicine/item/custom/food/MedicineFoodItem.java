92aae242f4f840de42c7a5236ff6a3efe3eb1ac4
rageniv <1311763909@qq.com>
2024-09-28 22:11:01 +0800

    为各个食物添加了未完成的提示信息

---
+++ src/main/java/com/medicine/item/custom/food/MedicineFoodItem.java
@@ -0,0 +1,19 @@
+package com.medicine.item.custom.food;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.text.Text;
+
+import java.util.List;
+
+public class MedicineFoodItem extends Item {
+    public MedicineFoodItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        tooltip.add(Text.translatable("tooltip.medicine.unfinished"));
+    }
+}
